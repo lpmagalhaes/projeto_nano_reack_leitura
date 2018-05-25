c3850b0ef7f8e19727533496b8cd3ec406e4a466
Leonardo Pereira Magalhaes <falecomleonardopereira@gmail.com>
2018-05-25 18:39:20 -0300

    Ordenação das postagens

--- src/components/Mural.js
+++ src/components/Mural.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
 import Postagem from './Postagem';
-import Categoria from './Categoria';
 import {Link} from 'react-router-dom';
 import {Alert, Button} from 'reactstrap';
 
 class Mural extends Component {
     state = {
-        categoriaSelecionada: null
+        categoriaSelecionada: null,
+        ordenacao: 'votos'
     }
     selecionarCategoria(categoria) {
         this.setState({categoriaSelecionada: categoria});
     }
+    selecionarOrdenacao(ordenacao) {
+        this.setState({ordenacao: ordenacao});
+    }
     render() {
         const {categorias, postagens} = this.props;
-        const {categoriaSelecionada} = this.state;
+        const {categoriaSelecionada, ordenacao} = this.state;
+        const corDefault = 'default';
+        const corSuccess = 'success';
+        const ordenacaoVotos = 'votos';
+        const ordenacaoData = 'data';
         let postagemParaMostrar = postagens;
-        if(categoriaSelecionada){
+        if (categoriaSelecionada) {
             postagemParaMostrar = postagemParaMostrar
                     .filter(postagem => (postagem.category === categoriaSelecionada));
         }
-        
+        let corBotaoOrdenacaoVotos = corDefault;
+        if (ordenacao === ordenacaoVotos) {
+            corBotaoOrdenacaoVotos = corSuccess;
+            postagemParaMostrar = postagemParaMostrar.sort((a, b) => (a.voteScore < b.voteScore));
+        }
+        let corBotaoOrdenacaoData = corDefault;
+        if (ordenacao === ordenacaoData) {
+            corBotaoOrdenacaoData = corSuccess;
+            postagemParaMostrar = postagemParaMostrar.sort((a, b) => (a.timestamp < b.timestamp));
+        }
         return (<div>
             <Alert color="primary">
                 Mural
@@ -31,31 +47,51 @@ class Mural extends Component {
             </Alert>        
             <Alert color="info">
                 Filtro &nbsp;
-                {categorias.map(categoria => {
-                    let corBotao = 'default';
-                    if(categoria.name === categoriaSelecionada){
-                        corBotao = 'success';
-                    }
-                    return <Button 
-                        color={corBotao}
-                        onClick={() => (this.selecionarCategoria(categoria.name))} 
-                        key={categoria.name}
-                        style={{'marginLeft': '5px'}}>
-                        {categoria.name}
-                    </Button>})}
-                    <Button 
-                        color='default'
-                        onClick={() => (this.selecionarCategoria(null))} 
-                        style={{'marginLeft': '5px'}}>
-                        Limpar Filtro
-                    </Button>
+                {
+                    categorias.map(categoria => {
+                        let corBotao = 'default';
+                        if (categoria.name === categoriaSelecionada) {
+                            corBotao = 'success';
+                }
+                return <Button 
+                    color={corBotao}
+                    onClick={() => (this.selecionarCategoria(categoria.name))} 
+                    key={categoria.name}
+                    style={{'marginLeft': '5px'}}>
+                    {categoria.name}
+                </Button>
+                })
+                }
+                <Button 
+                    color={corDefault}
+                    onClick={() => (this.selecionarCategoria(null))} 
+                    style={{'marginLeft': '5px'}}>
+                    Limpar Filtro
+                </Button>
             </Alert>
+            <Alert color="warning">
+                Ordenação
+                 <Button
+                    color={corBotaoOrdenacaoVotos}
+                    onClick={() => (this.selecionarOrdenacao('votos'))} 
+                    style={{'marginLeft': '5px'}}>
+                    Votos
+                </Button>
+                 <Button
+                    color={corBotaoOrdenacaoData}
+                    onClick={() => (this.selecionarOrdenacao('data'))} 
+                    style={{'marginLeft': '5px'}}>
+                    Data de Criação
+                </Button>        
+            </Alert>  
             <Alert>Postagens</Alert>
-            {postagemParaMostrar.filter(postagem => (postagem.deleted === false))
-                                        .map(postagem => (<Postagem key={postagem.id} postagem={postagem} />))}
-        
+                {
+                postagemParaMostrar
+                    .filter(postagem => (postagem.deleted === false))
+                    .map(postagem => (<Postagem key={postagem.id} postagem={postagem} />))
+                }        
         </div>);
+                }
             }
-        }
 
-        export default Mural;
+export default Mural;
